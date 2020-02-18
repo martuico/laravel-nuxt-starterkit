acdafc15cc8e826432a2ceae3a562e0d0bc5d9ad
Mar Tuico <mar.tuico@gmail.com>
2020-02-18 17:52:44 +0800

    Init

---
+++ laravel/docker/nginx-php/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+# jwilder/nginx-proxy support
+SERVER_NAME=${VIRTUAL_HOST:-${SERVER_NAME:-localhost}}
+
+envsubst '$SERVER_NAME $SERVER_ALIAS $SERVER_ROOT' < /nginx.conf.template > /etc/nginx/nginx.conf
+
+supervisord -c /supervisord.conf
